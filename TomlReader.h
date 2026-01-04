872eb52df4ef65aef7b9e6c4badd0b15b2d7065b
bogus <bogusz2005@gmail.com>
2026-01-04 23:31:23 +0100

    toml

---
+++ TomlReader.h
@@ -0,0 +1,145 @@
+//
+// Created by bogus on 2.12.2025.
+//
+
+#ifndef SPACE_INVADERS_TOMLREADER_H
+#define SPACE_INVADERS_TOMLREADER_H
+#include <fstream>
+#include <list>
+#include <toml++/toml.hpp>
+#include "SFML/Graphics/Texture.hpp"
+
+template<std::invocable<toml::table>... Fs>
+std::optional<std::tuple<std::invoke_result_t<Fs, toml::table>...> > create_form_toml(
+    const std::string &filename, Fs... fs) {
+    toml::table tbl;
+    try {
+        tbl = toml::parse_file(filename);
+    } catch (const toml::parse_error &err) {
+        return std::nullopt;
+    }
+    return std::make_tuple(fs(tbl)...);
+}
+
+// --- loaders ---
+
+struct Texture_TOML {
+    std::string name;
+    std::string texture;
+};
+
+inline std::vector<Texture_TOML> load_textures(const toml::table &tbl) {
+    std::vector<Texture_TOML> textures;
+
+    if (auto texture_array = tbl["Resources"]["texture"].as_array()) {
+        texture_array->for_each([&](auto &&el) {
+            if constexpr (toml::is_table<decltype(el)>) {
+                textures.emplace_back(el["name"].value_or(""), el["path"].value_or(""));
+            }
+        });
+    }
+
+    return textures;
+}
+
+struct Entity_TOML {
+    std::string name;
+    std::string texture;
+    int health;
+    int damage;
+};
+
+inline std::vector<Entity_TOML> load_enemy(const toml::table &tbl) {
+    std::vector<Entity_TOML> entity;
+
+    if (auto entity_array = tbl["Entity"]["enemy"].as_array()) {
+        entity_array->for_each([&](auto &&el) {
+            if constexpr (toml::is_table<decltype(el)>) {
+                entity.emplace_back(el["name"].value_or(""), el["texture"].value_or(""), el["health"].value_or(0),
+                                    el["damage"].value_or(0));
+            }
+        });
+    }
+
+    return entity;
+}
+
+inline std::vector<Entity_TOML> load_player(const toml::table &tbl) {
+    std::vector<Entity_TOML> entity;
+
+    if (auto entity_array = tbl["Entity"]["player"].as_array()) {
+        entity_array->for_each([&](auto &&el) {
+            if constexpr (toml::is_table<decltype(el)>) {
+                entity.emplace_back(el["name"].value_or(""), el["texture"].value_or(""), el["health"].value_or(0),
+                                    el["damage"].value_or(0));
+            }
+        });
+    }
+
+    return entity;
+}
+
+struct Level_TOML {
+    std::string name;
+    std::vector<std::vector<std::string> > layout;
+};
+
+inline std::vector<Level_TOML> load_level(const toml::table &tbl) {
+    std::vector<Level_TOML> levels;
+
+    if (auto level_array = tbl["Layout"]["level"].as_array()) {
+        level_array->for_each([&](auto &&el) {
+            if constexpr (toml::is_table<decltype(el)>) {
+                Level_TOML lvl;
+                lvl.name = el["name"].value_or("");
+
+                if (auto layout_array = el["layout"].as_array()) {
+                    for (auto &&row: *layout_array) {
+                        std::vector<std::string> row_vec;
+                        if (auto row_array = row.as_array()) {
+                            for (auto &&cell: *row_array) {
+                                if (cell.is_string())
+                                    row_vec.push_back(cell.value_or(""));
+                            }
+                        }
+                        lvl.layout.push_back(std::move(row_vec));
+                    }
+                }
+
+                levels.push_back(std::move(lvl));
+            }
+        });
+    }
+
+    return levels;
+}
+
+struct Game_TOML {
+    std::string name;
+    std::vector<std::string> levels;
+};
+
+inline std::vector<Game_TOML> load_game(const toml::table &tbl) {
+    std::vector<Game_TOML> levels;
+
+    if (auto level_array = tbl["Layout"]["game"].as_array()) {
+        level_array->for_each([&](auto &&el) {
+            if constexpr (toml::is_table<decltype(el)>) {
+                Game_TOML game;
+                game.name = el["name"].value_or("");
+
+                if (auto levels_array = el["levels"].as_array()) {
+                    for (auto &&levels_: *levels_array) {
+                        game.levels.push_back(levels_.value_or(""));
+                    }
+                }
+
+                levels.push_back(std::move(game));
+            }
+        });
+    }
+
+    return levels;
+}
+
+#endif //SPACE_INVADERS_TOMLREADER_H
--- main.cpp
+++ main.cpp
@@ -12,107 +12,120 @@
 #include <optional>
 
 #include "Button.h"
+#include "TomlReader.h"
 
 int main() {
 
-    const auto res = Res();
-
-    bool is_game_pause = true;
-
-    sf::RenderWindow window(sf::VideoMode({1000, 1000}), "Space Invaders", sf::Style::Close);
-    window.setPosition({10, 10});
-    window.setKeyRepeatEnabled(false);
-
-    Button start_game_button = Button([&]() {
-        is_game_pause = false;
-    });
-
-    start_game_button.setPosition({100,100});
-
-    std::vector<std::shared_ptr<Robot> > current_enemy = {};
-    Enemy_Controller enemy_controller(current_enemy,
-                                      {0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500},
-                                      {10, static_cast<float>(window.getSize().x) - 10},
-                                      res);
-
-    enemy_controller
-            .add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0)
-            .add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1)
-            .add_enemy(2).add_enemy(2).add_enemy(2).add_enemy(2)
-            .set_start_position();
-
-    std::vector<Wall> walls = {
-        {{50.0f, 800.0f}, {10, 10}, 5.0f},
-        {{350.0f, 800.0f}, {10, 10}, 5.0f},
-        {{650.0f, 800.0f}, {10, 10}, 5.0f},
-        {{950.0f, 800.0f}, {10, 10}, 5.0f},
-    };
-
-    Player player = Player({10, static_cast<float>(window.getSize().x) - 10}, 5, res);
-    set_y_for_player(player, window.getSize().y);
-
-    Bullet_Controller<Player, std::vector<std::shared_ptr<Robot> >, std::vector<Wall> > bullet_controller = {};
-    auto buller_helper_robot = bullet_controller.get_helper(player, std::nullopt, walls);
-    auto buller_helper_player = bullet_controller.get_helper(std::nullopt, current_enemy, walls);
+    auto toml = create_form_toml(
+        "resources/config.toml",
+        load_textures,
+        load_enemy,
+        load_player,
+        load_level,
+        load_game);
+    if (!toml) return 1;
+    auto& [textures,enemy,player,level,game] = toml.value();
 
 
 
-    sf::Clock clock;
-
-    while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            } else if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>()) {
-                if (!is_game_pause)
-                player.onKeyPressed(*keyPressed, buller_helper_player);
-
-                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
-                    is_game_pause = true;
-                }
-            } else if (const auto *keyReleased = event->getIf<sf::Event::KeyReleased>()) {
-                if (!is_game_pause)
-                player.onKeyReleased(*keyReleased);
-            } else if (const auto *mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
-                if (is_game_pause)
-                    start_game_button.onMouseButtonPressed(*mouseButtonPressed);
-            }
-        }
-        sf::Time restart = clock.restart();
-        if (!is_game_pause) {
-
-            player.update(restart);
-
-            enemy_controller.update(restart);
-
-            for (const auto &enemy: current_enemy) {
-                enemy->update(restart, player, buller_helper_robot);
-            }
-
-            bullet_controller.update(restart);
-
-            window.clear();
-
-            window.draw(player);
-
-            for (const auto &enemy: current_enemy) {
-                window.draw(*enemy);
-            }
-
-            window.draw(bullet_controller);
-
-            for (const auto &wall: walls) {
-                window.draw(wall);
-            }
-            window.display();
-
-        }else {
-            window.clear();
-
-            window.draw(start_game_button);
-
-            window.display();
-        }
-
-    }
+    // const auto res = Res();
+    //
+    // bool is_game_pause = true;
+    //
+    // sf::RenderWindow window(sf::VideoMode({1000, 1000}), "Space Invaders", sf::Style::Close);
+    // window.setPosition({10, 10});
+    // window.setKeyRepeatEnabled(false);
+    //
+    // Button start_game_button = Button([&]() {
+    //     is_game_pause = false;
+    // });
+    //
+    // start_game_button.setPosition({100,100});
+    //
+    // std::vector<std::shared_ptr<Robot> > current_enemy = {};
+    // Enemy_Controller enemy_controller(current_enemy,
+    //                                   {0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500},
+    //                                   {10, static_cast<float>(window.getSize().x) - 10},
+    //                                   res);
+    //
+    // enemy_controller
+    //         .add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0).add_enemy(0)
+    //         .add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1).add_enemy(1)
+    //         .add_enemy(2).add_enemy(2).add_enemy(2).add_enemy(2)
+    //         .set_start_position();
+    //
+    // std::vector<Wall> walls = {
+    //     {{50.0f, 800.0f}, {10, 10}, 5.0f},
+    //     {{350.0f, 800.0f}, {10, 10}, 5.0f},
+    //     {{650.0f, 800.0f}, {10, 10}, 5.0f},
+    //     {{950.0f, 800.0f}, {10, 10}, 5.0f},
+    // };
+    //
+    // Player player = Player({10, static_cast<float>(window.getSize().x) - 10}, 5, res);
+    // set_y_for_player(player, window.getSize().y);
+    //
+    // Bullet_Controller<Player, std::vector<std::shared_ptr<Robot> >, std::vector<Wall> > bullet_controller = {};
+    // auto buller_helper_robot = bullet_controller.get_helper(player, std::nullopt, walls);
+    // auto buller_helper_player = bullet_controller.get_helper(std::nullopt, current_enemy, walls);
+    //
+    //
+    //
+    // sf::Clock clock;
+    //
+    // while (window.isOpen()) {
+    //     while (const std::optional event = window.pollEvent()) {
+    //         if (event->is<sf::Event::Closed>()) {
+    //             window.close();
+    //         } else if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+    //             if (!is_game_pause)
+    //             player.onKeyPressed(*keyPressed, buller_helper_player);
+    //
+    //             if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+    //                 is_game_pause = true;
+    //             }
+    //         } else if (const auto *keyReleased = event->getIf<sf::Event::KeyReleased>()) {
+    //             if (!is_game_pause)
+    //             player.onKeyReleased(*keyReleased);
+    //         } else if (const auto *mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
+    //             if (is_game_pause)
+    //                 start_game_button.onMouseButtonPressed(*mouseButtonPressed);
+    //         }
+    //     }
+    //     sf::Time restart = clock.restart();
+    //     if (!is_game_pause) {
+    //
+    //         player.update(restart);
+    //
+    //         enemy_controller.update(restart);
+    //
+    //         for (const auto &enemy: current_enemy) {
+    //             enemy->update(restart, player, buller_helper_robot);
+    //         }
+    //
+    //         bullet_controller.update(restart);
+    //
+    //         window.clear();
+    //
+    //         window.draw(player);
+    //
+    //         for (const auto &enemy: current_enemy) {
+    //             window.draw(*enemy);
+    //         }
+    //
+    //         window.draw(bullet_controller);
+    //
+    //         for (const auto &wall: walls) {
+    //             window.draw(wall);
+    //         }
+    //         window.display();
+    //
+    //     }else {
+    //         window.clear();
+    //
+    //         window.draw(start_game_button);
+    //
+    //         window.display();
+    //     }
+    //
+    // }
 };
